2c82a529d7ccaed128b2d634cb3cdf213fc82720
Simrat Hayer <shayer@magnetforensics.com>
2024-09-15 20:17:06 -0400

    added mongo db url

--- models/db.js
+++ models/db.js
@@ -1,24 +1,31 @@
 // db.js
-const mongoose = require('mongoose');
-const AppData = require('./AppData'); // Ensure the path is correct
+const mongoose = require("mongoose");
+const AppData = require("./AppData");
 
-const localDB = `mongodb://0.0.0.0:27017/role_auth`;
+//const localDB = `mongodb://0.0.0.0:27017/role_auth`;
+
+const uri =
+  "mongodb+srv://wooble-db:wooble-db@cluster0.bqjnfql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const initializeAppData = async () => {
   try {
     const highestUserID = await AppData.findOne();
     if (!highestUserID) {
-      await AppData.create({highestUserID: 1});
+      await AppData.create({ highestUserID: 1 });
     }
   } catch (error) {
-    console.error('Error initializing app data:', error);
+    console.error("Error initializing app data:", error);
   }
 };
 
 const connectDB = async () => {
-  await mongoose.connect(localDB, {});
-  console.log('MongoDB Connected');
-  await initializeAppData();
+  try {
+    await mongoose.connect(uri, {});
+    console.log("Successfully connected to MongoDB!");
+    await initializeAppData();
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+  }
 };
 
 module.exports = connectDB;
