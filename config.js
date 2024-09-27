e7708c37462a46dd617ddd01abe61f7d28dcd8b4
Simrat Hayer <shayer@magnetforensics.com>
2024-09-27 14:40:22 -0400

    fixed server URL

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ const StripePublishableKey =
   "sk_test_51PqQlpD4UkX571U3Ov9ibcLrU3fqL4oamSp42vx8wl4PjNS0f1USJEkSz54uAbhhHALQ9pBqXghaVAKdiyFVvHwP00kDi3orE0";
 
 const PORT = 3000;
-const SERVER_URL = "http://localhost:3000";
+//const SERVER_URL = "http://localhost:3000";
+const SERVER_URL = "https://thebars.duckdns.org"; //for AWS EC2
 
 const TAXRATE = 0.13;
 const COMMISIONRATE = 0.08;
